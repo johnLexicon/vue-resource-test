6d6f35e4cf4fc4f1888750cef6d0da47cf596b81
john lundgren <john.lundgren.lexicon@gmail.com>
2021-05-27 09:43:27 +0200

    Fetching and rendering posts.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="container-fluid container-sm m-5">
+    <h1 class="text-primary mb-4">Fetching data with VueResource</h1>
+    <div v-if="posts" class="d-flex flex-column w-75">
+      <div v-for="post in posts" :key="post.id" class="card mb-3">
+        <div class="card-header">Id: {{ post.id }}</div>
+        <div class="card-body">
+          <h5 class="card-title">{{ post.title }}</h5>
+          <p class="card-text">
+            {{ post.body }}
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      posts: null,
+    };
+  },
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
+</script>
